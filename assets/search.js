e152f2f994c93f02c03b74159fb2e41e2129c644
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-07 15:23:17 +0000

    Deploy noproxy/noproxy.github.io to github.com/noproxy/noproxy.github.io.git:master

--- assets/search.js
+++ assets/search.js
@@ -1,3 +1,1 @@
----
----
-{%- include search-providers/default/search-data.js -%}
+window.TEXT_SEARCH_DATA={'posts':[{'title':"如何使用Gradle API获取项目的依赖信息",'url':"/2019/07/01/gradle-discovery-dependencies.html"},{'title':"ClassNotFoundException when Run Checkstyle Task by TestKit",'url':"/2019/07/05/gradle-issue-01.html"},{'title':"如何清理Mac磁盘空间",'url':"/2019/07/05/tips-mac-disk-clean.html"}]};
